0b3fb5b4b43565744f405b1c065f9495a9184652
seongmin <shengmin.jiang@outlook.com>
2026-03-03 13:50:58 +1100

    do module 5.2 ex2

--- course5.django/myproject/myapp/urls.py
+++ course5.django/myproject/myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [ 
     path('', views.home, name="home"), 
+    path('menu/', views.menu, name="menu"),
+    path('aboutus/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('drinks/<str:drink_name>', views.drinks, name="drinks"),
 ] 
--- course5.django/myproject/myapp/views.py
+++ course5.django/myproject/myapp/views.py
@@ -3,5 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1> Welcome to Little Lemon! </h1>")
+    return HttpResponse("Welcome to Little Lemon!")
 
+def about(request):
+    return HttpResponse("About us")
+
+def menu(request):
+    return HttpResponse("Menu")
+
+def book(request):
+    return HttpResponse("Make a booking")
+
+def drinks(request, drink_name):
+    drink_list = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of hot beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink_list[drink_name]
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
