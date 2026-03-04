8afea376783cfed8e779b65991e7ece531a46d49
seongmin <shengmin.jiang@outlook.com>
2026-03-04 11:26:41 +1100

    finish exercise 5.3.2

--- course5.django/myproject/myapp/admin.py
+++ course5.django/myproject/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Drinks
+from .models import Drinks, DrinksCategory
 
 # Register your models here.
 admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
