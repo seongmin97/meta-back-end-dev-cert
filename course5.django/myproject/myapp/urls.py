d6c3a4352d90ad1ff212d5c190cdd1f76012d667
seongmin <shengmin.jiang@outlook.com>
2026-03-11 16:42:31 +1100

    did exercies 5.4.3

--- course5.django/myproject/myapp/urls.py
+++ course5.django/myproject/myapp/urls.py
@@ -5,9 +5,14 @@ urlpatterns = [
     path('', views.home, name="home"), 
     # path('menu/', views.menu, name="menu"),
     # path('aboutus/', views.about, name="about"),
-    path('book/', views.book, name="book"),
+    # path('book/', views.book, name="book"),
     path('drinks/<str:drink_name>', views.drinks, name="drinks"),
     path('booking/', views.form_view, name="booking"),
-    path('menu/', views.menu),
-    path('about/', views.about),
+    # path('menu/', views.menu),
+    # path('about/', views.about),
+    path('home/', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('menu/', views.menu, name="menu"),
+    path('book/', views.book, name="book"),
+    
 ] 
--- course5.django/myproject/myapp/views.py
+++ course5.django/myproject/myapp/views.py
@@ -35,11 +35,25 @@ def form_view(request):
     context = {"form" : form}
     return render(request, "booking.html", context)
 
-def menu(request):
-    menu_item = Menu.objects.all()
-    item_dict = {"menu": menu_item}
-    return render(request, "menu.html", item_dict)
+# def menu(request):
+#     menu_item = Menu.objects.all()
+#     item_dict = {"menu": menu_item}
+#     return render(request, "menu.html", item_dict)
+
+# def about(request):
+#     about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+#     return render(request, "about.html", about_content)
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
 
 def about(request):
-    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
-    return render(request, "about.html", about_content)
+    return render(request, 'about.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def book(request):
+    return render(request, 'book.html')
